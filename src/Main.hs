22b29cebe9011d99c50966a737f810e6f833018c
José Vte. Calderón <jvcprod@hotmail.com>
2019-02-17 11:31:13 +0100

    Adds IO wrapper

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,24 @@
 module Main where
 
+import Data
+import MarsRover
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  boundaries <- getLine
+  rover
+  rover
+
+rover :: IO ()
+rover = do
+  position <- getLine
+  commPoolData <- getLine
+  if posFromStr position == Nothing
+    then do
+      putStrLn "Invalid position. Try again"
+      rover
+    else do
+      putStrLn . show $ (strCommPool (lift . posFromStr $ position) commPoolData)
+      putStr "\n"
+  where
+    lift (Just x) = x
