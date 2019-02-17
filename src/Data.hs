40a80a8416a4fcb3448a685f42468215291a72ad
José Vte. Calderón <jvcprod@hotmail.com>
2019-02-17 10:25:57 +0100

    Adds new data constructors

--- src/Data.hs
+++ src/Data.hs
@@ -1,6 +1,9 @@
 module Data where
 
-data Command = L | R | M deriving (Eq)
+import Text.Regex.PCRE
+import Data.List.Split
+
+data Command = L | R | M deriving (Show, Eq)
 
 data Cardinal = N | S | E | W deriving (Show, Eq)
 
@@ -10,8 +13,25 @@ instance Show Position where
   show (Pos x y c) = show x ++ " " ++ show y ++ " " ++ show c
   showsPrec = undefined
 
+cardFromChar :: Char -> Maybe Cardinal
+cardFromChar 'N' = Just N
+cardFromChar 'S' = Just S
+cardFromChar 'W' = Just W
+cardFromChar 'E' = Just E
+cardFromChar _ = Nothing
+
 commFromChar :: Char -> Maybe Command
 commFromChar 'L' = Just L
 commFromChar 'R' = Just R
 commFromChar 'M' = Just M
 commFromChar _   = Nothing
+
+posFromStr :: String -> Maybe Position
+posFromStr x
+  | isValid x = Just $ getPos x
+  | otherwise = Nothing
+  where
+    isValid = matchTest (makeRegex "^\\d+ \\d+ (N|S|E|W)$" :: Regex)
+    getPos = (\x -> Pos (toInt $ x!!0) (toInt $ x!!1) (lift . cardFromChar . head $ x!!2)) . splitOn " "
+    toInt x = (read x :: Int)
+    lift (Just x) = x
---
+++ test/DataSpec.hs
@@ -0,0 +1,22 @@
+module DataSpec where
+
+import Control.Exception (evaluate)
+import Data
+import Test.Hspec
+import Test.QuickCheck
+
+spec = do
+  describe "Mars rover algebraic data types" $ do
+    it "[cardFromChar] Should receive a char and maybe return a Cardinal" $ do
+      cardFromChar 'N' `shouldBe` Just N
+      cardFromChar 'S' `shouldBe` Just S
+      cardFromChar 'W' `shouldBe` Just W
+      cardFromChar 'E' `shouldBe` Just E
+      cardFromChar 'V' `shouldBe` Nothing
+    it "[commFromChar] Should receive a char and maybe return a Command" $ do
+      commFromChar 'L' `shouldBe` Just L
+      commFromChar 'R' `shouldBe` Just R
+      commFromChar 'M' `shouldBe` Just M
+    it "[posFromStr] Should receive a string and maybe return a Position" $ do
+      posFromStr "12 92 W" `shouldBe` Just (Pos 12 92 W)
+      posFromStr "12 wX" `shouldBe` Nothing
